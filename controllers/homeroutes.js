f85cf5b35c63a0b3cbfe27a0016c0940b92b6e58
gardnerd06 <115792714+gardnerd06@users.noreply.github.com>
2023-03-13 22:07:29 -0400

    added code for comments

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -18,6 +18,7 @@ router.get('/', async (req, res) => {
             posts,
             logged_in: req.session.logged_in,
         });
+
     } catch (err) {
         res.status(500).json(err);
     }
--- models/index.js
+++ models/index.js
@@ -4,21 +4,25 @@ const Comment = require('./comment');
 
 User.hasOne(Blog, {
     foreignKey: 'user_id',
+    constraints: false,
     onDelete: 'CASCADE',
 });
 
 Blog.belongsTo(User, {
     foreignKey: 'user_id',
+    constraints: false,
 });
 
 User.hasMany(Comment, {
     foreignKey: 'user_id',
+    constraints: false,
     onDelete: 'CASCADE',
 });
 
 
 Comment.belongsTo(User, {
     foreignKey: 'user_id',
+    constraints: false,
 });
 
 
